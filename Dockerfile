59bc15eaddcbe1153560e40efa888b1a4d0a54d1
a.lashnev <lexey2006.90@mail.ru>
2025-02-10 10:29:26 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.10-slim
+
+RUN apt-get update && apt-get install -y tesseract-ocr tesseract-ocr-rus
+
+WORKDIR /ocr
+
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . /ocr
+
+CMD ["sh", "-c", "cd app; uvicorn main:server --host 0.0.0.0 --port 4366 --workers 4"]
---
+++ app/main.py
@@ -0,0 +1,21 @@
+import pytesseract
+import base64
+import logging
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+from io import BytesIO
+from PIL import Image
+
+logging.getLogger().info("OCR run")
+server = FastAPI()
+
+class ImageRequest(BaseModel):
+    imageBase64: str
+
+@server.post("/image-to-text")
+def image_to_text(request: ImageRequest):
+    image_data = base64.b64decode(request.imageBase64)
+    image = Image.open(BytesIO(image_data))
+    text = pytesseract.image_to_string(image)
+    return JSONResponse(content={'text': text})
