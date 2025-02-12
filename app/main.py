415acdc8b8767c7484725073c4b7bc4cee8df9d8
a.lashnev <lexey2006.90@mail.ru>
2025-02-12 13:49:39 +0300

    Delete bad characters

--- app/main.py
+++ app/main.py
@@ -17,6 +17,18 @@ class ImageRequest(BaseModel):
 def image_to_text(request: ImageRequest):
     image_data = base64.b64decode(request.base64Image)
     image = Image.open(BytesIO(image_data))
-    rus_text = pytesseract.image_to_string(image, "rus")
-    eng_text = pytesseract.image_to_string(image, "eng")
+    rus_text = clean_text_russian(pytesseract.image_to_string(image, "rus"))
+    eng_text = clean_text_english(pytesseract.image_to_string(image, "eng"))
     return JSONResponse(content={'ruText': rus_text, 'enText': eng_text})
+
+import re
+
+def clean_text_russian(text):
+    text = re.sub(r'[^а-яА-ЯёЁ0-9\s,._!?«»–-]', '', text)
+    text = re.sub(r'\s+', ' ', text)
+    return text.strip()
+
+def clean_text_english(text):
+    text = re.sub(r'[^a-zA-Z0-9\s,._!?\'\"()–-]', '', text)
+    text = re.sub(r'\s+', ' ', text)
+    return text.strip()
